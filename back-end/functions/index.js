6411e4beac03da2fa835b99847d97f707600805f
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2020-09-22 19:08:16 +0300

    validations for signup users

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -24,6 +24,24 @@ var firebaseConfig = {
 
 const db = admin.firestore();
 
+const isEmpty = (string) => {
+   if (string.trim() === '') {
+      return true;
+   } else { 
+      return false;
+   }
+};
+
+const isEmail = (email) => {
+   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+   if (email.match(regEx)) {
+      return true;
+   } else {
+      return false;
+   }
+};
+
 app.get('/screams', (req, res) => {
    db
       .collection('screams')
@@ -68,6 +86,29 @@ app.post('/signup', (req, res) => {
       confirmPassword: req.body.confirmPassword,
       handle: req.body.handle
    };
+
+   let errors = {};
+   if (isEmpty(newUser.email)) {
+      errors.email = "Email must not be empty!";
+   } else if (!isEmail(newUser.email)) {
+      errors.email = "Email must be a valid email address!"
+   }
+
+   if (isEmpty(newUser.password)) { 
+      errors.password = 'Password must not be empty!'; 
+   }
+
+   if (newUser.password !== newUser.confirmPassword) {
+      errors.confirmPassword = 'Passwords must match!';
+   }
+
+   if (isEmpty(newUser.handle)) {
+      errors.handle = 'Handle must not be empty';
+   }
+
+   if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+   }
 
    let token, userId;
    db
