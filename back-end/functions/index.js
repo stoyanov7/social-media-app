47e429638a8be61509d651b2eaa5bf5d2eb9d68a
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2020-09-26 13:35:34 +0300

    get scream by id

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const firebaseAuth = require('./utils/firebaseAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream } = require('./handlers/screams');
 const { 
    signup, 
    login, 
@@ -13,6 +13,7 @@ const {
 
 app.get('/screams', getAllScreams);
 app.post('/scream', firebaseAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
 
 app.post('/signup', signup);
 app.post('/login', login);
