d2c62f4432aa4f45f1f2bdecc85ddb750938e2d7
stoyanov7 <georgi.stoyanov777@gmail.com>
2020-10-09 14:25:28 +0300

    initialize navbar

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>Test</div>
+        )
+    }
+}
+
+export default Navbar;
