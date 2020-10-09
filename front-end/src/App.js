b06a84ac44e0c71dda5985808fda3de4e9159347
stoyanov7 <georgi.stoyanov777@gmail.com>
2020-10-09 15:01:13 +0300

    add links and styes to navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './components/Navbar';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -9,13 +11,16 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Switch>
-         <Route exact path='/' component={Home} />
-         <Route exact path='/login' component={Login} />
-         <Route exact path='/signup' component={Signup} />
-       </Switch>
-     </Router>
+      <Router>
+        <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            </Switch>
+          </div>
+      </Router>
     </div>
   );
 }
