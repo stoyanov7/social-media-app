524db4b8fc28bfdf8bbe7e0739fb4f36fe1bcdbf
stoyanov7 <georgi.stoyanov777@gmail.com>
2020-10-09 16:14:58 +0300

    create mui theme provider

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,20 +8,42 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/signup' component={Signup} />
-            </Switch>
-          </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/signup' component={Signup} />
+              </Switch>
+            </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
