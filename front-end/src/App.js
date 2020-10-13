2f4993ab43e26137d895e7b5c5e91f9a7f8082b9
stoyanov7 <georgi.stoyanov777@gmail.com>
2020-10-13 10:47:46 +0300

    separate custom styles to file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,52 +10,9 @@ import Signup from './pages/Signup';
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './utils/theme';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  spreadThis: {
-    form: {
-      textAlign: 'center'
-    },
-    image: {
-      margin: '20px auto 20px auto'
-    }, 
-    pageTitle: {
-      margin: '10px auto 10px auto'
-    },
-    textField: {
-      margin: '10px auto 10px auto' 
-    },
-    button: {
-      marginTop: 20,
-      position: 'relative'
-    },
-    customError: {
-      color: 'red',
-      fontSize: '0.8rem',
-      marginTop: 10
-    },
-    progress: {
-      position: 'absolute'
-    }
-  }
-})
+const theme = createMuiTheme(themeFile);
 
 function App() {
   return (
