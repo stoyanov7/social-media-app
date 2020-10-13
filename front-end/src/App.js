fadbc8bd1f9b82003445f67f0c78543e6438776b
stoyanov7 <georgi.stoyanov777@gmail.com>
2020-10-13 11:52:38 +0300

    install jwt decode, create AuthRoute

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import './App.css';
+import themeFile from './utils/theme';
 
 import Navbar from './components/Navbar';
+import AuthRoute from './components/AuthRoute';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -10,9 +13,21 @@ import Signup from './pages/Signup';
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import themeFile from './utils/theme';
 
 const theme = createMuiTheme(themeFile);
+const token = localStorage.FBIdToken;
+let authenticated;
+
+if (token) {
+  const decodedToken = jwtDecode(token);
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
@@ -23,8 +38,18 @@ function App() {
             <div className="container">
               <Switch>
                 <Route exact path='/' component={Home} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={Signup} />
+                <AuthRoute 
+                  exact 
+                  path='/login' 
+                  component={Login} 
+                  authenticated={authenticated} 
+                />
+                <AuthRoute 
+                  exact 
+                  path='/signup' 
+                  component={Signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
         </Router>
